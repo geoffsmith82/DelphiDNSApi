690c4b9f190aa3bf6739dbf6b7746a5ae9726033
Geoffrey Smith <github@tysontechnology.com.au>
2025-12-07 12:28:26 +1100

    Update paths in DNSUI project file

--- Demos/DNSEditor/DNSUI.dpr
+++ Demos/DNSEditor/DNSUI.dpr
@@ -3,16 +3,16 @@ program DNSUI;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  DNS.Base in 'DNS.Base.pas',
-  DNS.Helpers in 'DNS.Helpers.pas',
-  DNS.Vultr in 'DNS.Vultr.pas',
-  DNS.DigitalOcean in 'DNS.DigitalOcean.pas',
-  DNS.Azure in 'DNS.Azure.pas',
   DNS.UI.Main in 'DNS.UI.Main.pas',
-  DNS.Cloudflare in 'DNS.Cloudflare.pas',
-  DNS.Google in 'DNS.Google.pas',
-  DNS.Route53 in 'DNS.Route53.pas',
-  DNS.Bunny in 'DNS.Bunny.pas';
+  DNS.Azure in '..\..\Source\DNS.Azure.pas',
+  DNS.Base in '..\..\Source\DNS.Base.pas',
+  DNS.Bunny in '..\..\Source\DNS.Bunny.pas',
+  DNS.Cloudflare in '..\..\Source\DNS.Cloudflare.pas',
+  DNS.DigitalOcean in '..\..\Source\DNS.DigitalOcean.pas',
+  DNS.Google in '..\..\Source\DNS.Google.pas',
+  DNS.Helpers in '..\..\Source\DNS.Helpers.pas',
+  DNS.Route53 in '..\..\Source\DNS.Route53.pas',
+  DNS.Vultr in '..\..\Source\DNS.Vultr.pas';
 
 {$R *.res}
 
